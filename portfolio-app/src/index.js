f10f95b595bc7c98e9a66ef605b8418bd12f5c08
emmagracekim <ekimsubs@gmail.com>
2022-02-25 15:04:55 -0800

    fixed

--- portfolio-app/src/index.js
+++ portfolio-app/src/index.js
@@ -4,19 +4,9 @@ import MyApp from "./MyApp"
 import "./index.css"
 import { BrowserRouter } from "react-router-dom"
 
-<<<<<<< HEAD
 ReactDOM.render(
   <BrowserRouter>
     <MyApp />
   </BrowserRouter>,
   document.getElementById("root")
 )
-=======
-
-
-
-
-ReactDOM.render(<MyApp />, document.getElementById("root"))
-
-
->>>>>>> 48cff056f17dea7e02121b27c4cfca853c583607
