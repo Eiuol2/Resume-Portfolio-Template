b966c7ad38ac2661fe590005d577b3c817422e5e
Eiuol2 <louiesabillo@gmail.com>
2022-03-04 14:56:58 -0800

    deleted duplicate DB.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
+module.exports = {
+    DB: process.env.JWT_DB,
+  };
+  
+
 mongoose.Promise = global.Promise;
 
 const url = process.env.MONGODB_URL;
