004a7548266abd4272d769d08a554fb4d6753c4a
Andrew Tran <andrewtran127@gmail.com>
2022-03-04 18:43:41 -0800

    Ignore this, using Emma's version

--- portfolio-app/src/components/Navigation.js
+++ portfolio-app/src/components/Navigation.js
@@ -1,50 +1,52 @@
 import { Navbar, Nav, Container } from "react-bootstrap"
 import React, { Component } from "react"
 import { Link } from "react-router-dom"
-
+import { LinkContainer } from "react-router-bootstrap"
 class Navigation extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <Link className="navbar-brand" to="/">
-            Resumix
-          </Link>
-          <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/resume">
-                  Resume
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/projects">
-                  Projects
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/profile">
-                  Profile
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/register">
-                  Register
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">
-                  Login
-                </Link>
-              </li>
-            </ul>
-          </div>
+      <Navbar
+        collapseOnSelect
+        expand="sm"
+        fixed="top"
+        className="bg-light justify-content-between"
+      >
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              Resumix
+              {/* <img src={my_image} alt='my alt text' className='profile-header'/> */}
+            </Navbar.Brand>
+          </LinkContainer>
 
-      </nav>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <LinkContainer to="/">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/resume">
+                <Nav.Link>Resume</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/projects" activeClassName="selectedMenuItem">
+                <Nav.Link>Projects</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/profile" activeClassName="selectedMenuItem">
+                <Nav.Link>Profile</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/register" activeClassName="selectedMenuItem">
+                <Nav.Link>Register</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login" activeClassName="selectedMenuItem">
+                <Nav.Link>Login</Nav.Link>
+              </LinkContainer>
+              {/* <Navbar.Text>
+                <Title />
+              </Navbar.Text> */}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     )
   }
 
@@ -91,7 +93,7 @@ class Navigation extends Component {
   //         </div>
   //       </nav>
   //     )
-  //   }
+  //   
 }
 
 export default Navigation
