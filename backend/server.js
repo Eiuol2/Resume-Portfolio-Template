a4b97ab980236018170f147aeb7b7e4a0a4d2102
Andrew Tran <andrewtran127@gmail.com>
2022-04-22 15:30:06 -0700

    Added Procfile

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ app.use("/profile", profileRoute);
 
 // PORT
 const port = 5016;
-const server = app.listen(port, () => {
+const server = app.listen(process.env.PORT || port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
 
